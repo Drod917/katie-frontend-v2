f92dd1dea44943d5f44069214b6f3d7b85907a9b
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-02-17 18:07:42 -0500

    Finished hero and sections responsive layout

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -7,14 +7,13 @@ const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-images">
-        <StaticImage style={{width: '60%'}} quality={100} src="../../content/images/hero1.jpg" alt="hero1" />
+        <StaticImage className="hero-1" quality={100} src="../../content/images/hero1.jpg" alt="hero1" />
         <div className="hero-circle" />
         <div className="hero-image-stack">
           <StaticImage 
             quality={100}
-            style={{height: '350px'}}
+            style={{minHeight: '350px'}}
             className="hero-2"
-            objectFit="cover" 
             src="../../content/images/hero2.jpg"
             alt="hero2" />
           <StaticImage quality={100} className="hero-3" src="../../content/images/hero3.jpg" alt="hero3" />
