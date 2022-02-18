0143aea9051d1aae5562eac3655c48be50126015
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-02-17 21:19:39 -0500

    Added ExtraInfo section, changed all font colors to a not-pure-black one

--- src/components/sections/ExtraInfo.js
+++ src/components/sections/ExtraInfo.js
@@ -2,10 +2,18 @@ import React from 'react';
 // import PropTypes from 'prop-types'
 import { StaticImage } from 'gatsby-plugin-image';
 
+import './ExtraInfo.scss';
+
 const ExtraInfo = props => {
   return (
     <div className='extra-info'>
-      <StaticImage />
+      <StaticImage src="../../content/images/extra-info-hero.jpg" alt="extra info hero" />
+      <div className='welcome-blurb'>
+        <div className='welcome-blurb-content'>
+          <h1>Welcome!</h1>
+          <p>New season, new wardrobe. As the temperatures continue to rise, have your style follow suit and revamp your racks with fresh, elevated summer looks.</p>
+        </div>
+      </div>
     </div>
   )
 }
