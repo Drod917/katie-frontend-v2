ef18f8215bef973948e5edb790f5cb9d23747a8d
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-02-18 23:50:29 -0500

    Finished mobile version minus contact form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,10 @@ import './Footer.scss';
 const Footer = () => {
 
     return (
-        <div>
-            <h1>Footer</h1>
+        <div className="footer-colored">
+            <div className="footer">
+                <h1>©2022 Katie Bostrom, LLC. All Rights Reserved</h1>
+            </div>
         </div>
     )
 }
--- src/components/sections/ExtraInfo.js
+++ src/components/sections/ExtraInfo.js
@@ -9,7 +9,7 @@ const ExtraInfo = props => {
     <>
       <div className="content-wrapper">
         <div className='extra-info'>
-          <StaticImage src="../../content/images/extra-info-hero.jpg" alt="extra info hero" />
+          <StaticImage quality={100} src="../../content/images/extra-info-hero.jpg" alt="extra info hero" />
           <div className='welcome-blurb'>
             <div className='welcome-blurb-content'>
               <h1>Welcome!</h1>
@@ -19,10 +19,10 @@ const ExtraInfo = props => {
           <div className="extra-info-blurb">
             <h1>Eyebrow Tinting</h1>
             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <StaticImage objectFit="contain" height={222} width={222} id="blurb-img-1" src="../../content/images/extra-info-blurb-1.jpg" alt="extra info blurb 1" />
+            <StaticImage quality={100} objectFit="contain" height={222} width={222} id="blurb-img-1" src="../../content/images/extra-info-blurb-1.jpg" alt="extra info blurb 1" />
           </div>
           <div className="extra-info-blurb">
-            <StaticImage objectFit="contain" height={222} width={222} src="../../content/images/extra-info-blurb-2.jpg" alt="extra info blurb 2" />
+            <StaticImage quality={100} objectFit="contain" height={222} width={222} src="../../content/images/extra-info-blurb-2.jpg" alt="extra info blurb 2" />
             <h1>Facials</h1>
             <p>Aliquam ut porttitor leo a. Augue interdum velit euismod in. Suscipit adipiscing bibendum est ultricies. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Tempus iaculis urna id volutpat lacus. Tortor at auctor urna nunc id cursus metus aliquam. Adipiscing diam donec adipiscing tristique risus nec feugiat.</p>
           </div>
@@ -31,7 +31,20 @@ const ExtraInfo = props => {
 
       <div className="bg-colored">
         <div className="content-wrapper">
-          EI2
+          <div className="extra-info-2">
+            <StaticImage className="img-square" quality={100} objectFit="contain" height={451} width={340} src="../../content/images/extra-info-blurb-3.jpg" alt="extra info blurb 3" />
+            <StaticImage quality={100} objectFit="contain" height={250} width={250} src="../../content/images/extra-info-blurb-4.jpg" alt="extra info blurb 4" />
+            <h1>Home skin care</h1>
+            <p>Vel fringilla est ullamcorper eget nulla facilisi. Nibh cras pulvinar mattis nunc. Massa id neque aliquam vestibulum morbi blandit. Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi. Id ornare arcu odio ut sem.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="content-wrapper">
+        <div className="extra-info-end">
+          <StaticImage quality={100} objectFit="contain" height={315} width={315} src="../../content/images/extra-info-blurb-5.jpg" alt="extra info blurb 5" />
+          <div className='vertical-line' />
+          <h1>the good ritual.</h1>
         </div>
       </div>
     </>
