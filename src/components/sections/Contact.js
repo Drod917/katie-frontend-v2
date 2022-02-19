80fec4ebf3039de02238dc393c59e5bf7d435af0
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-02-19 02:05:50 -0500

    Added basic contact form

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import './Contact.scss';
 
 const Contact = props => {
   return (
-    <div>Contact</div>
+    <div className="bg-colored">
+      <div className="content-wrapper">
+        <StaticImage 
+          quality={100}
+          style={{marginTop: '3em', height: '422px'}}
+          imgStyle={{clipPath: 'unset', height: '422px'}}
+          src="../../content/images/hero1.jpg"
+          alt="contact image"
+        />
+        <div className="contact-form-wrapper">
+          <div className="contact-form">
+            <h1>contact form</h1>
+            <input type="text" name="contactName" placeHolder="Enter your Name"/>
+            <input type="text" name="contactEmail" placeHolder="Enter a valid email address" />
+            <textarea placeHolder="Enter your message" />
+            <input type="button" value="Submit" />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
