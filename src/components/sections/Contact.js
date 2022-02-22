ac1c2c461bbd9bf6ac4585716e7ee9974f127cda
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-02-22 03:40:35 -0500

    Removed inline styling from gatsby StaticImages

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -7,20 +7,20 @@ const Contact = props => {
   return (
     <div className="bg-colored">
       <div className="content-wrapper">
-        <StaticImage 
-          quality={100}
-          style={{marginTop: '3em', height: '422px'}}
-          imgStyle={{clipPath: 'unset', height: '422px'}}
-          src="../../content/images/hero1.jpg"
-          alt="contact image"
-        />
-        <div className="contact-form-wrapper">
-          <div className="contact-form">
-            <h1>contact form</h1>
-            <input type="text" name="contactName" placeholder="Enter your Name"/>
-            <input type="text" name="contactEmail" placeholder="Enter a valid email address" />
-            <textarea placeholder="Enter your message" />
-            <input type="button" value="Submit" onClick={()=> (console.log('test'))}/>
+        <div className="col-wrapper">
+          <StaticImage 
+            quality={100}
+            src="../../content/images/hero1.jpg"
+            alt="contact image"
+          />
+          <div className="contact-form-wrapper">
+            <div className="contact-form">
+              <h1>contact form</h1>
+              <input type="text" name="contactName" placeholder="Enter your Name"/>
+              <input type="text" name="contactEmail" placeholder="Enter a valid email address" />
+              <textarea placeholder="Enter your message" />
+              <input type="button" value="Submit" onClick={()=> (console.log('test'))}/>
+            </div>
           </div>
         </div>
       </div>
