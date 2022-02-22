9d037f271f91c2ea623df0103091c6552adc8d28
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-02-22 00:37:14 -0500

    Finished tablet responsive design

--- src/components/sections/ExtraInfo.js
+++ src/components/sections/ExtraInfo.js
@@ -9,22 +9,24 @@ const ExtraInfo = props => {
     <>
       <div className="content-wrapper">
         <div className='extra-info'>
-          <StaticImage quality={100} src="../../content/images/extra-info-hero.jpg" alt="extra info hero" />
+          <StaticImage className="welcome-hero" quality={100} src="../../content/images/extra-info-hero.jpg" alt="extra info hero" />
           <div className='welcome-blurb'>
             <div className='welcome-blurb-content'>
               <h1>Welcome!</h1>
               <p>New season, new wardrobe. As the temperatures continue to rise, have your style follow suit and revamp your racks with fresh, elevated summer looks.</p>
             </div>
           </div>
-          <div className="extra-info-blurb">
-            <h1>Eyebrow Tinting</h1>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <StaticImage quality={100} objectFit="contain" height={222} width={222} id="blurb-img-1" src="../../content/images/extra-info-blurb-1.jpg" alt="extra info blurb 1" />
-          </div>
-          <div className="extra-info-blurb">
-            <StaticImage quality={100} objectFit="contain" height={222} width={222} src="../../content/images/extra-info-blurb-2.jpg" alt="extra info blurb 2" />
-            <h1>Facials</h1>
-            <p>Aliquam ut porttitor leo a. Augue interdum velit euismod in. Suscipit adipiscing bibendum est ultricies. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Tempus iaculis urna id volutpat lacus. Tortor at auctor urna nunc id cursus metus aliquam. Adipiscing diam donec adipiscing tristique risus nec feugiat.</p>
+          <div className="extra-info-blurb-wrapper">
+            <div className="extra-info-blurb">
+              <h1>Eyebrow Tinting</h1>
+              <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <StaticImage quality={100} objectFit="contain" height={222} width={222} id="blurb-img-1" src="../../content/images/extra-info-blurb-1.jpg" alt="extra info blurb 1" />
+            </div>
+            <div className="extra-info-blurb flipped">
+              <StaticImage quality={100} objectFit="contain" height={222} width={222} src="../../content/images/extra-info-blurb-2.jpg" alt="extra info blurb 2" />
+              <h1>Facials</h1>
+              <p>Aliquam ut porttitor leo a. Augue interdum velit euismod in. Suscipit adipiscing bibendum est ultricies. Molestie a iaculis at erat pellentesque adipiscing commodo elit at. Arcu dui vivamus arcu felis bibendum ut tristique et egestas. Tempus iaculis urna id volutpat lacus. Tortor at auctor urna nunc id cursus metus aliquam. Adipiscing diam donec adipiscing tristique risus nec feugiat.</p>
+            </div>
           </div>
         </div>
       </div>
@@ -32,10 +34,12 @@ const ExtraInfo = props => {
       <div className="bg-colored">
         <div className="content-wrapper">
           <div className="extra-info-2">
-            <StaticImage style={{marginTop: '4.3em', marginBottom: '1.8em'}} imgStyle={{clipPath: 'unset'}} quality={100} objectFit="contain" height={451} width={340} src="../../content/images/extra-info-blurb-3.jpg" alt="extra info blurb 3" />
-            <StaticImage quality={100} objectFit="contain" height={250} width={250} src="../../content/images/extra-info-blurb-4.jpg" alt="extra info blurb 4" />
-            <h1>Home skin care</h1>
-            <p>Vel fringilla est ullamcorper eget nulla facilisi. Nibh cras pulvinar mattis nunc. Massa id neque aliquam vestibulum morbi blandit. Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi. Id ornare arcu odio ut sem.</p>
+            <StaticImage className="square-img" quality={100} objectFit="cover" src="../../content/images/extra-info-blurb-3.jpg" alt="extra info blurb 3" />
+            <div className="extra-info-2-blurb-wrapper">
+              <StaticImage quality={100} objectFit="contain" height={250} width={250} src="../../content/images/extra-info-blurb-4.jpg" alt="extra info blurb 4" />
+              <h1>Home skin care</h1>
+              <p>Vel fringilla est ullamcorper eget nulla facilisi. Nibh cras pulvinar mattis nunc. Massa id neque aliquam vestibulum morbi blandit. Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi. Id ornare arcu odio ut sem.</p>
+            </div>
           </div>
         </div>
       </div>
@@ -43,8 +47,10 @@ const ExtraInfo = props => {
       <div className="content-wrapper">
         <div className="extra-info-end">
           <StaticImage quality={100} objectFit="contain" height={315} width={315} src="../../content/images/extra-info-blurb-5.jpg" alt="extra info blurb 5" />
-          <div className='vertical-line' />
-          <h1>the good ritual.</h1>
+          <div className='extra-info-end-blurb-wrapper'>
+            <div className='vertical-line' />
+            <h1>the good ritual.</h1>
+          </div>
         </div>
       </div>
     </>
