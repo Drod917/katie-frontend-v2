a3af08c0e76c328219444bb6f3dbe7cf09bf0ccc
Daniel Rodriguez <drodriguez11375@gmail.com>
2022-04-06 11:57:06 -0400

    Added bootstrap 5 css+js

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -7,7 +7,10 @@ const SEO = props => {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Open+Sans:wght@700&family=Playfair+Display:wght@400;700&family=Raleway:ital,wght@0,400;0,500;0,700;1,300&display=swap" rel="stylesheet"/> 
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+      </script>
     </Helmet>
   )
 }
